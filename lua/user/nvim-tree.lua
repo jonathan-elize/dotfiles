11eb35a9ec11e6c3f395205dc36c801869f5aeed
Jonathan Elize <jonathan.elize@gmail.com>
2022-01-04 21:11:59 -0500

    tweaked ui to not show bufferline and not show line in folder in nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -20,11 +20,18 @@ vim.g.nvim_tree_icons = {
   },
 }
 vim.g.nvim_tree_quit_on_open = 1 --0 by default, closes the tree when you open a file
-vim.g.nvim_tree_indent_markers = 1 --0 by default, this option shows indent markers when folders are open
+vim.g.nvim_tree_indent_markers = 0 --0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_highlight_opened_files = 3 --0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_add_trailing = 1; --0 by default, append a trailing slash to folder names
 vim.g.nvim_tree_group_empty = 1; --0 by default, compact folders that only contain a single folder into one node in the file tree
 vim.g.nvim_tree_root_folder_modifier = ':t'; --TODO find a modifier that looks better
+vim.g.nvim_tree_show_icons = {
+	git= 1,
+	folders= 1,
+	files= 1,
+	folder_arrows= 0, --TODO find better folder arrrow icons
+}
+--TODO Find a way to highlight the row you're
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -94,12 +101,5 @@ nvim_tree.setup {
   git_hl = 1,
   disable_window_picker = 0,
   root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
 }
 

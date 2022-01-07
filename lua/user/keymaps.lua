af50fd597dd793fd62e83a9a49b3f89e4a1f3ba1
Jonathan Elize <jonathan.elize@gmail.com>
2022-01-07 02:22:14 -0500

    Added more to support commit

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -83,6 +83,7 @@ keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 keymap("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 
 --Format
 keymap("n", "<leader>fd", ":Format<cr>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -111,6 +111,9 @@ return packer.startup(function(use)
 	--toggleterminal
 	use "akinsho/toggleterm.nvim"
 
+	--project
+	use "ahmedkhalf/project.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

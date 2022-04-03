b3d727f1803ef24b54caf9223eb011baa6fe265b
Jonathan Elize <jonathan.elize@gmail.com>
2022-04-03 13:07:24 -0500

    Added vimspector

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -120,6 +120,9 @@ return packer.startup(function(use)
 	--whichkey
 	use "folke/which-key.nvim"
 
+	--debugger
+	use 'puremourning/vimspector'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
